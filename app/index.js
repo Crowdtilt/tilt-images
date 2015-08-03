b00a5da0fc1d177ba2e9ef0785f6bd3730ee8ef2
Brett Bukowski <brett.bukowski@gmail.com>
2015-08-03 16:21:17 -0700

    Adds icons for Audience, Money, Platforms, TiltLogoOutline

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ var include = function(componentName) {
     exports[componentName] = require('./components/' + componentName);
 };
 
+include('Audience');
 include('Camera');
 include('Cards');
 include('Checkmark');
@@ -16,14 +17,17 @@ include('Gift');
 include('GmailLogo');
 include('Group');
 include('Lock');
+include('Money');
 include('OneLeft');
 include('Options');
 include('PaperAirplaneImage');
 include('PermissionEmailEnvelope');
 include('PermissionFriends');
+include('Platforms');
 include('Star');
 include('TagFriend');
 include('TiltLogo');
+include('TiltLogoOutline');
 include('TiltLogoTextImage');
 include('Triangle');
 include('TwitterLogo');
