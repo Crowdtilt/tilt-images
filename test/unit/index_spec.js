7ed1e72b91652a77b4adf3a035aa1c663dd15126
Dave King <tildedave@gmail.com>
2016-08-18 08:36:06 -0700

    fix tests to work with shallow rendering

--- test/unit/index_spec.js
+++ test/unit/index_spec.js
@@ -3,12 +3,12 @@ import TestUtils from 'react-addons-test-utils';
 import TiltImages from '../../app/components';
 import ReactDOM from 'react-dom';
 
-describe('TiltImages', function () {
-  Object.keys(TiltImages).forEach(function (componentName) {
-    describe(componentName, function () {
-      it('should render without error', function () {
+describe('TiltImages', () => {
+  Object.keys(TiltImages).forEach(componentName => {
+    describe(componentName, () => {
+      it('should render without error', () => {
         const Component = TiltImages[componentName];
-        const e = TestUtils.renderIntoDocument(<Component />);
+        const e = TestUtils.renderIntoDocument(<div><Component /></div>);
         const text = ReactDOM.findDOMNode(e).querySelector('title').textContent;
         expect(text).to.equal(componentName);
       });
